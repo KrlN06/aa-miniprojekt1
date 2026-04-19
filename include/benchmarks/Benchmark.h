6bc1a5c3075a4e9380f3c7ecc3157d8dd81c363c
KrlN06 <krl.nalepa@yahoo.com>
2026-04-20 00:41:30 +0200

    Benchmark start

--- include/benchmarks/Benchmark.h
+++ include/benchmarks/Benchmark.h
@@ -62,56 +62,7 @@ private:
     void displayResults(int size);
 };
 
-template<typename T>
-void Benchmark<T>::run() {
-
-    for (double percent : percents) {
-
-        for (int size : sizes) {
-
-
-            for (int i = 0; i < 100; i++) {
-
-
-                //Common datasets
-                std::vector<T> data = ArrayGenerator<T>::generatePartiallySortedData(size, percent);
-
-                //Arrays to be sorted by different algorithms
-                std::vector<T> quickSortData = data;
-                std::vector<T> mergeSortData = data;
-                std::vector<T> introSortData = data;
-
-
-
-                //Run benchmarks
-                benchmarkQuickSort(quickSortData);
-                benchmarkMergeSort(quickSortData);
-                benchmarkIntroSort(quickSortData);
-
-                displayResults(size);
-
-            }
-            quickSortResultsAverages.push_back(calculateAverage(quickSortResults));
-            mergeSortResultsAverages.push_back(calculateAverage(mergeSortResults));
-            introSortResultsAverages.push_back(calculateAverage(introSortResults));
-
-            quickSortResults.clear();
-            mergeSortResults.clear();
-            introSortResults.clear();
-
-
-        }
-    }
-
-}
-
-
-
-
-
-
-
-
+#include "Benchmark.tpp"
 
 
 #endif //PROJEKT_1_BENCHMARK_H
--- include/generators/ArrayGenerator.h
+++ include/generators/ArrayGenerator.h
@@ -17,47 +17,7 @@ private:
     static std::mt19937 gen;
 };
 
-template<typename T>
-std::mt19937 ArrayGenerator<T>::gen(std::random_device{}());
-
-
-template<typename T>
-int ArrayGenerator<T>::generateRandomInt(int min, int max) {
-    std::uniform_int_distribution<int> dist(min, max);
-    return dist(gen);
-}
-
-
-
-template<typename T>
-std::vector<T> ArrayGenerator<T>::generateRandomData(int size) {
-    return generatePartiallySortedData(size, 0.0);
-}
-
-
-template <typename T>
-std::vector<T> ArrayGenerator<T>::generatePartiallySortedData(int size, double sortedPercent) {
-
-    std::vector<T> data;
-
-    if (sortedPercent == 0.0) {
-        for (int i = 0; i < size; i++) {
-            data.push_back(static_cast<T>(generateRandomInt(0, 9999)));
-        }
-        return data;
-    }
-    int sortedIndexes = static_cast<int>(size * sortedPercent);
-
-    for (int i = 0; i < sortedIndexes; i++) {
-        data.push_back(static_cast<T>(i));
-    }
-    int randomIndexes = size - sortedIndexes;
-    for (int i = 0; i < randomIndexes; i++) {
-        data.push_back(static_cast<T>(generateRandomInt(0, 9999)));
-    }
-    return data;
-}
-
+#include "ArrayGenerator.tpp"
 
 
 
